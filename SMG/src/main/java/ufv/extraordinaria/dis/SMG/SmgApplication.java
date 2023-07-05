b151f039b9bf71b297ad86a3a19a9f08d91d41d4
Sergio <sergiomedinagomez2014@gmail.com>
2023-07-05 15:29:53 +0200

    clase tweet

---
+++ SMG/src/main/java/ufv/extraordinaria/dis/SMG/SmgApplication.java
@@ -0,0 +1,13 @@
+package ufv.extraordinaria.dis.SMG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmgApplication.class, args);
+	}
+
+}
